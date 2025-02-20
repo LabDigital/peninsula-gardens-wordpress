265e749c272b8d61d2ae796ade3d24e5e17db952
Rakshit Maniar <rrmaniar28@gmail.com>
2025-02-20 19:24:17 +0530

    GP-661 update plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInita0d847a362130a7402ff139f93a85779::getLoader();
+return ComposerAutoloaderInit89688acf207ec0d984adf485b3045e72::getLoader();
